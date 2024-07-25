fd09b8fbeb3099942439123aa80d4e17bba152a4
TankChirag1212 <tankchirag17@gmail.com>
2024-07-25 17:20:38 +0530

    changed App.java file on the dev branch

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,1 +1,7 @@
-System.out.println("Hello From Development Branch!");
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello From Development Branch!");
+    }
+}
