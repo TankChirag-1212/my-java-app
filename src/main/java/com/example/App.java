d56168274fbf31a51b61b08593dccb7bcd1f3727
TankChirag1212 <tankchirag17@gmail.com>
2024-07-25 15:55:15 +0530

    changed app.java file

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,8 +1,1 @@
-package com.example;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, Maven!");
-    }
-}
-
+System.out.println("Feature Branch 1");
